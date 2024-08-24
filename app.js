d0b01f3cf75afa9032e12faa6db2d91a50cb62a3
silviabarbieri <110141846+silviabarbieri@users.noreply.github.com>
2024-08-24 10:48:27 -0300

    Primeros pasos del proyecto

---
+++ app.js
@@ -0,0 +1,69 @@
+const textarea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+// La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+   const textoEncriptado = encriptar(textarea.value)
+   mensaje.value = textoEncriptado
+   textarea.value="";
+   mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada){ 
+let matrizCodigo = [["e", "enter"],["i", "imes"],["a","ai"],["o","ober"],["u","ufat"]];
+stringEncriptada = stringEncriptada.toLowerCase()
+
+for(let i=0; i < matrizCodigo.length; i++){
+    if(stringEncriptada.includes(matrizCodigo[i][0])){
+        stringEncriptada=stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+    }
+}
+return stringEncriptada
+}
+
+function desencriptar(stringDesencriptada){ 
+    let matrizCodigo = [["e", "enter"],["i", "imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+    
+    for(let i=0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada=stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+    }
+
+    function btnDesencriptar(){
+        const textoEncriptado = desencriptar(textarea.value)
+        mensaje.value = textoEncriptado
+        textarea.value="";
+     }
+
+     //funcion copiar
+     document.addEventListener('DOMContentLoaded', function() {
+        const copiarBtn = document.getElementById('copiarBtn');
+        const mensaje = document.getElementById('mensaje');
+    
+        copiarBtn.addEventListener('click', function() {
+            // Selecciona el texto en el área de texto
+           mensaje.select();
+            mensaje.setSelectionRange(0, 99999); // Para dispositivos móviles
+            try {
+                // Copia el texto al portapapeles
+                document.execCommand('copy');
+                alert('Texto copiado al portapapeles');
+            } catch (err) {
+                alert('No se pudo copiar el texto: ' + err);
+            }
+        });
+    });
+
+    
+    
+    
+    
